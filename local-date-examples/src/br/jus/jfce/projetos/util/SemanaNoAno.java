f6a4d28fa3061b37ca7c90d37a083971de31e13b
Vinicius Dantas e Melo <vdmelo@gmail.com>
2019-01-23 12:17:11 -0300

    Adicionando métodos para permitir escolher o dia em que a semana se inicia

--- local-date-examples/src/br/jus/jfce/projetos/util/SemanaNoAno.java
+++ local-date-examples/src/br/jus/jfce/projetos/util/SemanaNoAno.java
@@ -1,4 +1,5 @@
 package br.jus.jfce.projetos.util;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.TemporalField;
 import java.time.temporal.WeekFields;
@@ -8,6 +9,18 @@ import java.time.temporal.WeekFields;
  * 
  * */
 public class SemanaNoAno {
+
+	/**
+	 * Recebe um LocalDate e retorna a semana no ano.
+	 * Neste caso, o primeiro dia da primeira semana de 2019 é 01/01/2019
+	 * 
+	 * Nesta versão do método, você pode decidir o dia que inicia a semana.
+	 */
+	public static int pegarSemanaDoAno(LocalDate date, DayOfWeek dow) {
+		TemporalField woy = WeekFields.of(dow, 1).weekOfYear();
+		return date.get(woy);
+
+	}
 
 	/**
 	 * Recebe um LocalDate e retorna a semana no ano.
@@ -24,8 +37,7 @@ public class SemanaNoAno {
 	public static void main(String[] args) {
 
 		/**
-		 * Imprime uma tabela com todos os dias de JANEIRO 
-		 * e sua respectiva semana no ano 
+		 * Imprime uma tabela com todos os dias de JANEIRO e sua respectiva semana no ano  
 		 */
 		LocalDate janeiro = LocalDate.of(2019, 1, 1);
 		DatePrinter.printHeader(janeiro);
@@ -34,15 +46,28 @@ public class SemanaNoAno {
 			DatePrinter.print(date, pegarSemanaDoAno(date));
 		}
 
+		
+		/**
+		 * Imprime uma tabela com todos os dias de JANEIRO e sua respectiva semana no ano
+		 * Mas dessa vez, vamos colocar que a semana se inicia na segunda-feira e não no Domingo (padrão do Locale pt_BR)
+		 */
+		LocalDate janeiro2 = LocalDate.of(2019, 1, 1);
+		DatePrinter.printHeader(janeiro2);
+		for (int i = 0; i < 31; i++) {
+			LocalDate date = janeiro2.plusDays(i);
+			DatePrinter.print(date, pegarSemanaDoAno(date, DayOfWeek.MONDAY));
+		}
+
+		
 		/**
 		 * Imprime uma tabela com todos os dias de FEVEREIRO 
 		 * e sua respectiva semana no ano 
-		 */
+		 * /
 		LocalDate fevereiro = LocalDate.of(2019, 2, 1);
 		DatePrinter.printHeader(fevereiro);
 		for (int i = 0; i < 28; i++) {
 			LocalDate date = fevereiro.plusDays(i);
 			DatePrinter.print(date, pegarSemanaDoAno(date));
-		}
+		}*/
 	}
 }
--- local-date-examples/src/br/jus/jfce/projetos/util/SemanaNoMes.java
+++ local-date-examples/src/br/jus/jfce/projetos/util/SemanaNoMes.java
@@ -1,4 +1,5 @@
 package br.jus.jfce.projetos.util;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.TemporalField;
 import java.time.temporal.WeekFields;
@@ -8,6 +9,18 @@ import java.time.temporal.WeekFields;
  * 
  * */
 public class SemanaNoMes {
+
+	/**
+	 * Recebe um LocalDate e retorna a semana no ano.
+	 * Neste caso, o primeiro dia da primeira semana de 2019 é 01/01/2019
+	 * 
+	 * Esta versão do método permite definir em que dia a semana se inicia
+	 */
+	public static int pegarSemanaDoMes(LocalDate date, DayOfWeek dow) {
+		TemporalField wom = WeekFields.of(dow, 1).weekOfMonth();
+		return date.get(wom);
+	}
+
 	/**
 	 * Recebe um LocalDate e retorna a semana no ano.
 	 * Neste caso, o primeiro dia da primeira semana de 2019 é 01/01/2019
@@ -32,15 +45,27 @@ public class SemanaNoMes {
 			DatePrinter.print(date, pegarSemanaDoMes(date));
 		}
 
+		
+		/**
+		 * Imprime uma tabela com todos os dias de JANEIRO e sua respectiva semana no ano  
+		 * Desta vez, vamos usar a segunda-feira para iniciar a semana
+		 */
+		LocalDate janeiro2 = LocalDate.of(2019, 1, 1);
+		DatePrinter.printHeader(janeiro2);
+		for (int i = 0; i < 31; i++) {
+			LocalDate date = janeiro2.plusDays(i);
+			DatePrinter.print(date, pegarSemanaDoMes(date, DayOfWeek.MONDAY));
+		}
+
 		/**
 		 * Imprime uma tabela com todos os dias de FEVEREIRO 
 		 * e sua respectiva semana no ano 
-		 */
+		 * /
 		LocalDate fevereiro = LocalDate.of(2019, 2, 1);
 		DatePrinter.printHeader(fevereiro);
 		for (int i = 0; i < 28; i++) {
 			LocalDate date = fevereiro.plusDays(i);
 			DatePrinter.print(date, pegarSemanaDoMes(date));
-		}
+		}*/
 	}
 }
